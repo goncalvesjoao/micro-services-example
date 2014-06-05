89f4f45605b55567a41fbf20288d32f0321d6495
João Gonçalves <goncalves.joao@gmail.com>
2014-06-05 15:14:28 +0100

    headers with CSRF_TOKEN

--- rails-consumer-app/app/models/smooth_resource.rb
+++ rails-consumer-app/app/models/smooth_resource.rb
@@ -1,17 +1,28 @@
 class SmoothResource < SmoothOperator::Rails
 
-  def self.before_request(http_verb, relative_path, data, options)
-    options[:headers] ||= {}
+  def self.headers
+    headers = super
 
-    if current_user
-      options[:headers].merge!({
-        cookie: current_user.blog_cookie,
-        "authenticity_token" => current_user.blog_auth_token
-      })
-    end
-
-    [http_verb, relative_path, data, options]
+    headers.merge({
+      cookie: current_user.blog_cookie,
+      "X_CSRF_TOKEN" => current_user.blog_auth_token
+    })
   end
+
+  # IF you don't want to add the user_id on the controller,
+  # you can always added it on every :put or :post requests
+
+  # def self.before_request(http_verb, relative_path, data, options)
+  #   if [:put, :patch, :post].include?(http_verb)
+  #     data[resource_name] ||= {}
+
+  #     if current_user
+  #       data[resource_name][:user_id] = current_user.id
+  #     end
+  #   end
+
+  #   [http_verb, relative_path, data, options]
+  # end
 
   protected ############## PROTECTED #################
 
